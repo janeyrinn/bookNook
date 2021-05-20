9d391784d97179058c6fd7ba3eaf11b3e2c97516
janeyrinn <jane.rinn@gmail.com>
2021-05-20 16:52:49 +0000

    Fix punctuation in intro text

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,16 @@
 // initializes book icon to open sidenav
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    edge: "right"
   });
+});
 
 // generates date for copywrite in footer
 $("#copyright").text(new Date().getFullYear());
 
 // facilitates typewriter text effect on homepage(accredited to W3 Schools)
-var i =0;
-var txt = 'welcome to your little haven for book reviews and recommendations find your next favourite book or share a current literary love comment on reviews and engage with other readers';
+var i = 0;
+var txt = 'welcome to your little haven for book reviews and recommendations, find your next favourite book or share a current literary love, comment on reviews and engage with other readers.';
 var speed = 60;
 
 function typeWriterEffect() {
