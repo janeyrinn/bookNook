a19c221f5f488ff6f20c7c9f02cd611b33f8f73f
janeyrinn <jane.rinn@gmail.com>
2021-06-01 19:39:27 +0000

    changer error names to 404

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ def delete_comment(comment_id):
 @app.errorhandler(404)
 def page_not_found(e):
 
-    return render_template('error.html', e=e), 404
+    return render_template('404.html', e=e), 404
 
 
 if __name__ == '__main__':
