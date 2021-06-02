6b459eaf558e631abc2d9161bb69fa81ed3047e5
janeyrinn <jane.rinn@gmail.com>
2021-06-02 17:31:34 +0000

    add scroll to top button

--- static/js/script.js
+++ static/js/script.js
@@ -25,3 +25,24 @@ function typeWriterEffect() {
 $(document).ready(function(){
   $('.modal').modal();
 });
+
+// code found on w3 schools and customized
+
+mybtn = document.getElementById("mybtn");
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybtn.style.display = "block";
+  } else {
+    mybtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
