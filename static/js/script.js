efbcfd473befe133f3c9e3fef4bbc6345385ec99
janeyrinn <jane.rinn@gmail.com>
2021-06-04 15:35:38 +0000

    remove typewriter effect

--- static/js/script.js
+++ static/js/script.js
@@ -8,19 +8,6 @@ $(document).ready(function () {
 // generates date for copywrite in footer
 $("#copyright").text(new Date().getFullYear());
 
-// facilitates typewriter text effect on homepage(accredited to W3 Schools)
-var i = 0;
-var txt = 'welcome to your little haven for book reviews and recommendations, find your next favourite book or share a current literary love, comment on reviews and engage with other readers.';
-var speed = 60;
-
-function typeWriterEffect() {
-  if (i < txt.length) {
-    document.getElementById('intro').innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriterEffect, speed);
-  }
-}
-
 // inititializes delete review/comment modal : Materialize
 $(document).ready(function(){
   $('.modal').modal();
